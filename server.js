0ac957c6e0e5b9574d5bbafaacd42be16e563e57
Worash <uppert83@gmail.com>
2022-02-27 06:39:43 -0800

    serverjs file added

---
+++ server.js
@@ -0,0 +1,35 @@
+const express=require('express')
+const app=express()
+const bodyParser=require('body-parser')
+const cors=require('cors')
+const path=require('path')
+const compression=require('compression')
+const {port}=require('./config/config')
+const route=require('./route')
+const socketIo = require("socket.io");
+const { decrptObject } = require('./auth/encrypt')
+const { disconnect } = require('process')
+const { webSocket } = require('./socket')
+app.use('/static',express.static(path.join(__dirname,'public')));
+
+// compress all response
+app.use(compression())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(cors())
+/** routes */
+app.use(route)
+/**--socket connection---- */
+const server = require('http').createServer(app);
+const io = socketIo(server, {
+   cors: {
+     origin: "http://192.168.8.101:3000",
+     methods: ["GET", "POST" , "PUT"],
+   }
+ });
+webSocket(io)
+server.listen(port,err=>{
+   if (err) console.log(err)
+   console.log('Server up and running on localhost:'+port)
+})
+
